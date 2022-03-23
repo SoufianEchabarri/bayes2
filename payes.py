cfa5685282e84ccfc0b420033eae29e2a496a644
SoufianEchabarri <96385012+SoufianEchabarri@users.noreply.github.com>
2022-03-23 15:27:03 +0100

    Update payes.py

--- payes.py
+++ payes.py
@@ -1,3 +1,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import scipy as sp
+
+def dist(nchain, init, propsd, X, Y):
+  chain=np.zeros((nchain+1, 5)
+  gamma=init[2]
+  lambd=init[3]
+  tau=init[4]
+  chain[0]=init
+   alpha=init[0]
+  beta=init[1]              
+                 
